b061a6b3f2793762abd49a9c0772defbba09b66a
Baitemir <ferrari170604@gmail.com>
2021-05-16 15:43:34 +0600

    filled project overview

--- src/components/Site/Projects/Project/Project.js
+++ src/components/Site/Projects/Project/Project.js
@@ -6,13 +6,13 @@ const Project = ({match}) => {
 
     switch (match.params.id) {
       case "music-maker":
-        project_name = "MM"
+        project_name = "Music maker"
         break;
       case "personal-site":  
-        project_name = "PS"
+        project_name = "Personal site"
         break;
       case "kanye-western":  
-        project_name = "KW"
+        project_name = "Kanye Western"
         break;
       
       default:
@@ -22,6 +22,11 @@ const Project = ({match}) => {
   return ( <div className={classes.Project}>
       <div className={classes.info}>
         <h1>{project_name}</h1>
+        <p>What is Lorem Ipsum? Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. Why do we use it? It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
+        <ul>
+          <li><a href=''>GitHub repository</a></li>
+          <li><a href='https://www.baitemir.netlify.app' target='_blank'>Netlify link</a></li>
+        </ul>
       </div>
       <div className={classes.img}>
 

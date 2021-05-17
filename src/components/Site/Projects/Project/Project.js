ac583360d05b6a11496099f581ca9f7f559f213d
Baitemir <ferrari170604@gmail.com>
2021-05-17 18:44:55 +0600

    stylized project page

--- src/components/Site/Projects/Project/Project.js
+++ src/components/Site/Projects/Project/Project.js
@@ -1,5 +1,6 @@
 import classes from "./Project.module.css";
 import mus_img from "../../../../images/music-maker.png";
+import kw_img from "../../../../images/kanye-western.png";
 
 const Project = ({match}) => {
 
@@ -10,28 +11,25 @@ const Project = ({match}) => {
     switch (match.params.id) {
       case "music-maker":
         project_name = "Music maker"
-        links.push(<ul>
+        links.push(<ul className={classes.links}>
           <li><a href='https://github.com/BaitemirAsanbaev/music-maker' rel="noreferrer" target='_blank'>GitHub repository</a></li>
-          <li><a href='https://baitemir-asanbaev.netlify.app/' rel="noreferrer" target='_blank'>Netlify link</a></li>
         </ul>)
-        screenshot.push(<img alt="s" className={classes.screenshot} src={mus_img}/>)
-        
+        screenshot.push(<a href='https://baitemir-asanbaev.netlify.app/' rel="noreferrer" target='_blank'><img alt="s" className={classes.screenshot} src={mus_img}/></a>)
+
         break;
       case "personal-site":  
         project_name = "Personal site"
-        links.push(<ul>
+        links.push(<ul className={classes.links}>
           <li><a href='https://github.com/BaitemirAsanbaev/personal-site' rel="noreferrer" target='_blank'>GitHub repository</a></li>
-          <li><a href='https://baitemir.netlify.app/' rel="noreferrer" target='_blank'>Netlify link</a></li>
         </ul>)
-        screenshot.push(<img alt="s" className={classes.screenshot} src={mus_img}/>)
+        screenshot.push(<a href='https://baitemir.netlify.app/' rel="noreferrer" target='_blank'><img alt="s" className={classes.screenshot} src={mus_img}/></a>)
         break;
       case "kanye-western":  
         project_name = "Kanye Western"
-        links.push(<ul>
+        links.push(<ul className={classes.links}>
           <li><a href='https://github.com/BaitemirAsanbaev/kanye-western' rel="noreferrer" target='_blank'>GitHub repository</a></li>
-          <li><a href='https://kanye-western.netlify.app/' rel="noreferrer" target='_blank'>Netlify link</a></li>
         </ul>)
-        screenshot.push(<img alt="s" className={classes.screenshot} src={mus_img}/>)
+        screenshot.push(<a href='https://kanye-western.netlify.app/' rel="noreferrer" target='_blank'><img alt="s" className={classes.screenshot} src={kw_img}/></a>)
         break;
       
       default:
@@ -42,10 +40,11 @@ const Project = ({match}) => {
       <div className={classes.info}>
         <h1>{project_name}</h1>
         <p>What is Lorem Ipsum? Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. Why do we use it? It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
-      {links}
+        {links}
       </div>
       <div className={classes.img}>
         {screenshot}
+        <span>Visit site</span>
       </div>
   </div> );
 }

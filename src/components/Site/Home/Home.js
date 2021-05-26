0df601cc265994f93b7436f935a9ee22552dc701
Baitemir <ferrari170604@gmail.com>
2021-05-26 22:31:35 +0600

    replaced lorems

--- src/components/Site/Home/Home.js
+++ src/components/Site/Home/Home.js
@@ -1,6 +1,7 @@
 import { useSelector } from "react-redux";
 import ProjectItem from "../Projects/ProjectsList/ProjectItem/ProjectItem";
 import classes from "./Home.module.css";
+import avatar from "../../../images/baitemir.jpg"
 
 const Home = () => {
 
@@ -36,10 +37,13 @@ const Home = () => {
 
   return (<div className={classes.Home}>
     <div className={classes.home_about}>
-      <div className={classes.img}></div>
+      <div className={classes.img}>
+        <img alt="ava" src={avatar}/>
+      </div>
       <div className={classes.info}>
         <h1>Baitemir Asanbaev</h1>
-        <p>What is Lorem Ipsum? Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. Why do we use it? It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.
+        <p>
+          Hi there. I'm Baitemir and I'm web developer. I create great web-sites using ReactJS. Also I'm always ready to learn new technologies and fields. 
         </p>
       </div>
     </div>
--- src/components/Site/Projects/Project/Project.js
+++ src/components/Site/Projects/Project/Project.js
@@ -6,39 +6,114 @@ import ap_img from "../../../../images/apple.png";
 
 const Project = ({match}) => {
 
-  let project_name = "";
+  let project_name = "Project '?'";
+  let project_description = "unckown project please check the URL";
   const links = [];
-  let screenshot = [];
+  const screenshot = [];
 
     switch (match.params.id) {
       case "music-maker":
-        project_name = "Music maker"
+        project_name = "Music maker";
+
+        project_description = "Music-maker where you can create great melodies and save it. Or listen music already exists. This app was created with ReactJS, based on Firebase and hosted on Netlify ";
+
         links.push(<ul className={classes.links}>
-          <li><a href='https://github.com/BaitemirAsanbaev/music-maker' rel="noreferrer" target='_blank'>GitHub repository</a></li>
-        </ul>)
-        screenshot.push(<a href='https://baitemir-asanbaev.netlify.app/' rel="noreferrer" target='_blank'><img alt="s" className={classes.screenshot} src={mus_img}/></a>)
+            <li><a
+            href='https://github.com/BaitemirAsanbaev/music-maker'
+            rel="noreferrer"
+            target='_blank'>
+              GitHub repository
+            </a></li>
+          </ul>);
+
+        screenshot.push(<a
+          href='https://baitemir-asanbaev.netlify.app/'
+          rel="noreferrer" target='_blank'>
+            <img alt="s"
+            className={classes.screenshot}
+            src={mus_img}/>
+          </a>);
 
         break;
-      case "personal-site":  
+
+        
+      case "personal-site":
+
         project_name = "Personal site"
+
+        project_description = "My personal site where you can find information about me and see my projects. Btw you are already here))";
+
         links.push(<ul className={classes.links}>
-          <li><a href='https://github.com/BaitemirAsanbaev/personal-site' rel="noreferrer" target='_blank'>GitHub repository</a></li>
-        </ul>)
-        screenshot.push(<a href='https://baitemir.netlify.app/' rel="noreferrer" target='_blank'><img alt="s" className={classes.screenshot} src={ps_img}/></a>)
+          <li><a
+          href='https://github.com/BaitemirAsanbaev/personal-site'
+          rel="noreferrer"
+          target='_blank'>
+            GitHub repository
+            </a></li>
+        </ul>);
+
+        screenshot.push(<a
+          href='https://baitemir.netlify.app/'
+          rel="noreferrer"
+          target='_blank'>
+            <img alt="s"
+              className={classes.screenshot}
+              src={ps_img}/>
+          </a>);
         break;
-      case "kanye-western":  
+
+
+      case "kanye-western": 
+
         project_name = "Kanye Western"
+
+        project_description = "Web-game made using JavaScript. In this game you will shoot Kenye Wests head and collect bullets. If you waste all your bullets, you'll have to listen Kanye Wests song but in Spanish!";
+
         links.push(<ul className={classes.links}>
-          <li><a href='https://github.com/BaitemirAsanbaev/kanye-western' rel="noreferrer" target='_blank'>GitHub repository</a></li>
-        </ul>)
-        screenshot.push(<a href='https://kanye-western.netlify.app/' rel="noreferrer" target='_blank'><img alt="s" className={classes.screenshot} src={kw_img}/></a>)
+          <li><a
+          href='https://github.com/BaitemirAsanbaev/kanye-western'
+          rel="noreferrer"
+          target='_blank'>
+            GitHub repository
+            </a></li>
+        </ul>);
+
+        screenshot.push(<a
+          href='https://kanye-western.netlify.app/'
+          rel="noreferrer"
+          target='_blank'>
+            <img
+              alt="s"
+              className={classes.screenshot}
+              src={kw_img}/>
+            </a>);
         break;
+
+
       case "apple-copy":  
+
         project_name = "Apple copy"
+
+        project_description = "Layout of Apple's website. I used only HTML and CSS to made it.";
+
         links.push(<ul className={classes.links}>
-          <li><a href='https://github.com/BaitemirAsanbaev/apple-copy' rel="noreferrer" target='_blank'>GitHub repository</a></li>
-        </ul>)
-        screenshot.push(<a href='https://apple-layout.netlify.app/' rel="noreferrer" target='_blank'><img alt="s" className={classes.screenshot} src={ap_img}/></a>)
+          <li><a
+          href='https://github.com/BaitemirAsanbaev/apple-copy'
+          rel="noreferrer"
+          target='_blank'>
+            GitHub repository
+          </a></li>
+        </ul>);
+
+        screenshot.push(<a
+          href='https://apple-layout.netlify.app/'
+          rel="noreferrer"
+          target='_blank'>
+            <img
+              alt="s"
+              className={classes.screenshot}
+              src={ap_img}/>
+            </a>);
         break;
       
       default:
@@ -48,7 +123,7 @@ const Project = ({match}) => {
   return ( <div className={classes.Project}>
       <div className={classes.info}>
         <h1>{project_name}</h1>
-        <p>What is Lorem Ipsum? Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. Why do we use it? It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
+        <p>{project_description}</p>
         {links}
       </div>
       <div className={classes.img}>

b205bbf9d48cef18f9af59b3fac91a299d4735a1
Baitemir <ferrari170604@gmail.com>
2021-05-27 11:53:43 +0600

    SEO

--- src/components/Site/About/About.js
+++ src/components/Site/About/About.js
@@ -6,7 +6,7 @@ const About = () => {
     <div className={classes.about_info}>
       <h2>Let's get acquainted</h2>
       <p>
-      Hi there. I'm Baitemir and I'm web developer. I create great web-sites using ReactJS. Also I'm always ready to learn new technologies and fields. For now my skills are: HTML5, CSS3, JavaScript, ReactJS, Redux, Router-DOM, Ajax, GitHub, Firebase, Figma, Adobe Illustrator, Asobe Photoshop. Skills I'll learn soon: React Native, Python/Django, WordPress, Adobe AfterEffects, PHP.<br/>
+      Hi there. I'm Baitemir and I'm web developer. I create great web-sites using ReactJS. Also I'm always ready to learn new technologies and fields. For now my skills are: HTML5, CSS3, JavaScript, ReactJS, Redux, Router-DOM, Ajax, GitHub, Firebase, Figma, Adobe Illustrator, Asobe Photoshop. Skills I'll learn soon: React Native, Python/Django, WordPress, Adobe AfterEffects, PHP, SEO.<br/>
       </p>
     </div>
   </div> );
